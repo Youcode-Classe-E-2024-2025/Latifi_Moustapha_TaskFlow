9fca6b26ff10ee63ad6e62e2fa75b03c4deda934
latifi777 <abdelalilatifi3@gmail.com>
2024-12-26 15:38:00 +0100

    en commencant la partie login

--- config/config.php
+++ config/config.php
@@ -1,30 +1,32 @@
 <?php
-
 class Database {
     // proprite de la connexion
-    private $host = "localhost";
-    private $dbname = "taskFlow";
-    private $username = "root" ;
-    private $password = "";
-    private $connexion ;
+        private $host = "localhost";
+        private $db_name = "taskFlow";
+        private $username = "root";
+        private $password = "";
+        private $connexion;
 
-    //  methode de la connexion
-    public function getconnexion(){
-        $this->connexion = null ;
+        // Méthode pour obtenir la connexion
+        public function getConnection() {
+            $this->connexion = null;
 
-        try{
-            $connexion = new PDO (
-                "mysql:host=".$this->host. "; dbname=".$this->dbname ,
-                $this->username,
-                $this->password
-            ) ;
-            $this->connexion->setAtrribute(PDO::ATRR_ERRMODE, PDO::ERRMODE_EXEPTION);
-            $this->connexion->setAtrribute(PDO:: ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        }catch (PDOEXEPTION $e){
-            "Error connexion : " .get.meassage($e);
+            try {
+                $this->connexion = new PDO(
+                    "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                    $this->username,
+                    $this->password
+                );
+                $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                echo 'Connexion successful' ;
+            } catch (PDOException $e) {
+                echo "Connexion failed : " . $e->getMessage();
+            }
+
+            return $this->connexion;
         }
-        return $this->connexion ;
     }
 
 
-}
+?>
